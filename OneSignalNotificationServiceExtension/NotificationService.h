e5d97add7394b8bd503fc7c002cf4d25002e1039
Nan <nanftw@gmail.com>
2022-02-09 12:42:25 -0800

    Add the NSE

---
+++ OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by Nan Li on 2/9/22.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
